049648f739fede0a1c9c91685047e68d93b66203
Visheshjha11 <visheshjha456@gmail.com>
2025-07-04 21:48:24 +0530

    cpp basic code of todays lecture

--- assingment.cpp
+++ assingment.cpp
@@ -21,6 +21,33 @@ using namespace std;
 //     cout << "My Name" <<endl <<endl << "potato\n"<<endl<<"and"<<"IEat"<<"Tomato";
 // }
 
-int main(){
+// int main(){
+//     // decimal to binary conversion
+//     int n, i = 0, binary[32];
+//     cout << "Enter a decimal number: ";
+//     cin >> n;
+//     while (n > 0) {
+//         binary[i] = n % 2;
+//         n = n / 2;
+//         i++;
+//     }
+//     cout << "Binary representation: ";
+//     for (int j = i - 1; j >= 0; j--) {
+//         cout << binary[j];
+//     }
+
+// }
+
+// int main(){
+//     int x;
+//     cout << x;
+//     return 0;
+
     
-}
+// }
+
+// int main(){
+//     int $ab =2;
+//     cout<<$ab;
+//     return 0;
+// }
